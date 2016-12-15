9aef108f2eff54861be9193bab06ad3e4fdfcf5f
ac <adrien.cadoret@vif.fr>
2016-12-15 17:51:51 +0100

    Building of the navigation bar

--- app/src/main/java/fr/cadoret/adrien/care/MainActivity.java
+++ app/src/main/java/fr/cadoret/adrien/care/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_myday) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_weight) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_measurements) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_photos) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_sport_activity) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
